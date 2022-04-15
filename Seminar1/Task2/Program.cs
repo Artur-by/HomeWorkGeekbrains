44044c6b447f9921bcc9da7ef317900a83b7b4c7
Artur-by <artur.by@mail.ru>
2022-04-15 17:00:03 +0300

    Second task solved

--- Seminar1/Task2/Program.cs
+++ Seminar1/Task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// 
-Console.WriteLine("Hello, Artur!");
+﻿//программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел
+
+double NumA = 22;
+double NumB = 5;
+double NumC = 9;
+if (NumA >= NumB)
+{
+    if (NumA >= NumC)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(NumA);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.WriteLine(NumC);
+    }
+}
+else
+{
+    if (NumB >= NumC)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(NumB);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.WriteLine(NumC);
+    }
+}
+
