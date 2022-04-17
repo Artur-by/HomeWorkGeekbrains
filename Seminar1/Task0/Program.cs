a1894cd90bcad6209f0f0b19616c9d5f5a0357f7
Artur-by <artur.by@mail.ru>
2022-04-17 16:40:21 +0300

    Class work solved

---
+++ Seminar1/Task0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 
+// В зависимости ои введенной цифры вывести день недели.
+// Предполагаем, что введенные цифры целые
+
+int N = 7;
+if (N <= 0) Console.WriteLine(" Такого дня недели не существует");
+if (N > 7) Console.WriteLine(" Такого дня недели не существует");
+if (N == 1) Console.WriteLine("Понедельник");
+if (N == 2) Console.WriteLine("Вторник");
+if (N == 3) Console.WriteLine("Среда");
+if (N == 4) Console.WriteLine("Четверг");
+if (N == 5) Console.WriteLine("Пятница");
+if (N == 6) Console.WriteLine("Суббота");
+if (N == 7) Console.WriteLine("Воскресенье");
