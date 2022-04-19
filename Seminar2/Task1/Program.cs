480799842bc2d583659b24a37c877206a29b070b
Artur-by <artur.by@mail.ru>
2022-04-19 15:42:45 +0300

    Task1 solved

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа
+
+int N = new Random().Next(10, 99);
+Console.WriteLine(N);
+
+int NumA = N / 10;
+int NumB = N % 10;
+
+if (NumA > NumB)
+{
+    Console.WriteLine(NumA);
+}
+else
+{
+    Console.WriteLine(NumB);
+}
