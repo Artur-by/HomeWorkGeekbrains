abfb5850e863deedc2f09a0f30bb1da60a40018c
Artur-by <artur.by@mail.ru>
2022-04-20 23:17:21 +0300

    Task8 add blok-diagram N2 with test program

--- Seminar2/Task8/Program.cs
+++ Seminar2/Task8/Program.cs
@@ -3,11 +3,11 @@
 
 int[] array = { -100, -10, -7, -5, -3, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15 };
 
-void Days(int days)
+void Days(int day)
 {
-    if (days > 0 && days < 8)
+    if (day > 0 && day < 8)
     {
-        if (days < 6)
+        if (day < 6)
         {
             Console.WriteLine("Нет");
         }
