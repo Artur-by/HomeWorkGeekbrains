deb67c4dd41f742fe2ccf6c753abe6f58f588663
Artur-by <artur.by@mail.ru>
2022-04-20 12:13:50 +0300

    Task 7-2 solved

---
+++ Seminar2/Task7/Program.cs
@@ -0,0 +1,33 @@
+﻿// программf, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+
+// Функция Count() подсчитывает на какое число необходимо разделить
+// введенное число чтобы осталось только три знака
+
+int Count(int number)
+{
+    int count = 1000;
+    while (number - count > 0)
+    {
+        count = count * 10;
+    }
+    count = count / 1000;
+    return count;
+}
+
+int NumA = -992365897;
+if (NumA < 0) NumA = -NumA;
+
+if (NumA < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    int count = Count(NumA);
+    Console.WriteLine((NumA / count) % 10);
+}
+
+
+
