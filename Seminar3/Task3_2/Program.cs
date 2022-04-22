b2640cda0cf903317e7fde91eb0a9cd7e5d94072
Artur-by <artur.by@mail.ru>
2022-04-22 17:17:13 +0300

    Task 3-2 solved

---
+++ Seminar3/Task3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве
+
+int Ax = 7;
+int Ay = -5;
+int Bx = 1;
+int By = -1;
+
+int len = Ax - Bx;
+int wid = Ay - By;
+
+double Square(int len, int wid)
+{
+    return Math.Sqrt(len * len + wid * wid);
+
+}
+double S = Square(len, wid);
+Console.Write(Math.Round(S, 2));
