2e4f31d086bb0113a6b9aeb6b4c1be2495c9715c
Artur-by <artur.by@mail.ru>
2022-04-24 16:58:46 +0300

    Task3_6 solved with cycle FOR

---
+++ Seminar3/Task3_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int Cube(int i)
+{
+    return i * i * i;
+}
+
+int num = 5;
+
+if (num < 0) num = -num;
+for (int i = 1; i <= num; i++)
+{
+    int cub = Cube(i);
+    Console.WriteLine(cub);
+}
