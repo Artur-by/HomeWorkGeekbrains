11dbfe0a8b5a029900b9fd1ade9fb28a12b8398f
Artur-by <artur.by@mail.ru>
2022-04-24 15:55:36 +0300

    Rename Task3-4 to Task3_4

---
+++ Seminar3/Task3_4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом - например 12521.
+
+string Compare(int N)
+{
+    int num1 = N / 10000;
+    int num2 = (N / 1000) % 10;
+    int num4 = (N % 100) / 10;
+    int num5 = N % 10;
+    string result;
+
+    if (num1 == num5 && num2 == num4) result = "Да";
+    else result = "Нет";
+    return result;
+
+}
+
+int N = 42324;
+Console.Write(Compare(N));
