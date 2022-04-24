6b4a07af6fcab5c5d4f358532b0361db722e46c6
Artur-by <artur.by@mail.ru>
2022-04-24 15:16:53 +0300

    Task 3-3 solved

---
+++ Seminar3/Task3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N
+
+int NumA = 7;
+
+int Square(int num)
+{
+    return num * num;
+}
+
+if (NumA < 0) NumA = -NumA;
+int ind = 1;
+while (ind <= NumA)
+{
+    Console.WriteLine(Square(ind));
+    ind++;
+}
