a3ea6ec66182e9598cd9bad2ef8690125ea898b4
Artur-by <artur.by@mail.ru>
2022-04-24 17:19:53 +0300

    Task3_1 corrected code

--- Seminar3/Task3_1/Program.cs
+++ Seminar3/Task3_1/Program.cs
@@ -5,16 +5,10 @@ int Quart(int x, int y)
 {
     if (x > 0 && y > 0) return 1;
     if (x < 0 && y > 0) return 2;
-    if (x < 0 && y < 0)
-    {
-        return 3;
-    }
-    else
-    {
-        return 4;
-    }
+    if (x < 0 && y < 0) return 3;
+    else return 4;
 }
 
 int x = -15;
-int y = 10;
+int y = -10;
 Console.Write(Quart(x, y));
