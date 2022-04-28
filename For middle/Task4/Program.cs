f64259295c576896fe8ad6fae5f0c273eec831fa
Artur-by <artur.by@mail.ru>
2022-04-28 14:57:09 +0300

    Task 4 solved

---
+++ For middle/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+double A = 2;
+int B = 10;
+
+double Digree(double A, int B)
+{
+    double result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+Console.WriteLine($"{A} в сепени {B} равно {Digree(A, B)}");
