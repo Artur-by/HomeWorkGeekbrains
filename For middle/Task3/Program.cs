c03c79c026834e097b2f881dc42831f9bdd37b01
Artur-by <artur.by@mail.ru>
2022-04-28 08:58:52 +0300

    Task3 solved where A - char AscII

---
+++ For middle/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Найти сумму чисел от 1 до А
+
+int nummin = 1;
+char nummax = 'A';
+
+int FormatTab(char num)
+{
+    int nummax = Convert.ToInt16(num);
+    return nummax;
+}
+
+int[] CreatTab(int nummax)
+{
+    int[] tab = new int[nummax];
+    return tab;
+}
+
+int[] FillTab(int[] tab)
+{
+    for (int i = 0; i < tab.Length; i++)
+    {
+        tab[i] = i + 1;
+    }
+    return tab;
+}
+
+int SumTab(int[] tab)
+{
+    int sumTab = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        sumTab = tab[i] + sumTab;
+    }
+    return sumTab;
+}
+
+
+
+int num = FormatTab(nummax);
+int[] tab = CreatTab(num);
+FillTab(tab);
+int sum = SumTab(tab);
+
+
+Console.WriteLine($"сумма чисел от {nummin} до {nummax}({num}) равна {sum}");
+
