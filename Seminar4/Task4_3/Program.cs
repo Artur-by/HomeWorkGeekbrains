6020a41b6323250d8e535b9352b6d921c1c62657
Artur-by <artur.by@mail.ru>
2022-05-01 21:09:54 +0300

    Task 4_3 solved

---
+++ Seminar4/Task4_3/Program.cs
@@ -0,0 +1,39 @@
+﻿//Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int N = 8;
+
+int[] CreatTable(int N)
+{
+    int[] table = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = i + 1;
+    }
+    return table;
+}
+
+
+int[] AddTable(int[] table)
+{
+    for (int i = 0; i < table.Length; i++)
+    {
+        table[i] = new Random().Next(-999, 999);
+    }
+    return table;
+}
+
+
+
+void ReadTable(int[] table)
+{
+    for (int i = 0; i < table.Length; i++)
+    {
+        Console.Write($" {table[i]} |");
+    }
+}
+Console.WriteLine();
+
+int[] tab = CreatTable(N);
+tab = AddTable(tab);
+Console.WriteLine("Массив из 8 цифр в диапазоне от -999 до 999 :");
+ReadTable(tab);
