781c746eef71188ded8a79f542dded5652985614
Artur-by <artur.by@mail.ru>
2022-05-01 21:33:56 +0300

    task Necessarily solved without cycle

---
+++ Seminar4/Necessarily/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите целое положительное число А: ");
+string s = Console.ReadLine();
+double A = Convert.ToDouble(s);
+
+double result = A * (A + 1) / 2;
+System.Console.WriteLine($"Сумма чисел от 1 до {A} равна {result}");
