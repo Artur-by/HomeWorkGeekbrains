0daff83f274be2f9c4073645019a9d19dd54992f
Artur-by <artur.by@mail.ru>
2022-05-01 17:54:57 +0300

    Task 6 solved with convert int-string-int

---
+++ For middle/Task6/Program.cs
@@ -0,0 +1,34 @@
+﻿// Подсчитать сумму цифр в числе
+
+decimal N = 1000002.012546m;
+
+// метод преобразования отрицательного числа в положительное
+decimal Controle(decimal N)
+{
+    if (N < 0) return -N;
+    else return N;
+}
+
+// метод преобразования числа в строку
+string Convert(decimal N)
+{
+    string n = N.ToString();
+    return n;
+}
+
+//метод подсчета суммы символов в строке без учета запятой
+// перевод в int - int.Parse(n.ToString())
+int Sum(string N)
+{
+    int sumN = 0;
+    for (int i = 0; i < N.Length; i++)
+    {
+        if (N[i] != ',') sumN = sumN + int.Parse(N[i].ToString());
+
+    }
+    return sumN;
+}
+
+decimal n = Controle(N);
+string strN = Convert(n);
+Console.WriteLine($"в числе {N} сумма цифр {Sum(strN)}");
