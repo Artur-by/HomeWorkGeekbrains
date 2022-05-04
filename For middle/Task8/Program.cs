110f47df2464428232dd84e3a9a44dc989a86494
Artur-by <artur.by@mail.ru>
2022-05-04 17:04:40 +0300

    Task 8 For middle solved

---
+++ For middle/Task8/Program.cs
@@ -0,0 +1,43 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите количество элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArr(int N)
+{
+    int[] array = new int[N];
+    return array;
+}
+
+int[] FillArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        array[i] = array[i] * array[i] * array[i];
+    }
+    return array;
+}
+
+void ReadeArr(int[] array)
+{
+    Console.WriteLine("Исходный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} |");
+    }
+}
+
+void CubeArr(int[] arr)
+{
+    Console.WriteLine("Кубы чисел, заканчивающихся на четную цифру ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) Console.Write($"{arr[i]} |");
+    }
+}
+
+int[] array = CreateArr(N);
+FillArr(array);
+ReadeArr(array);
+Console.WriteLine();
+CubeArr(array);
