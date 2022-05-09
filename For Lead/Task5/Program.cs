7fde00265156a5e4491751094e215c0be659c76e
Artur-by <artur.by@mail.ru>
2022-05-09 22:09:21 +0300

    Task5 For Lead solved

---
+++ For Lead/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать числа Фибоначчи
+Console.WriteLine("Числа Фибоначчи: ");
+
+double first = 0;
+double second = 1;
+Console.WriteLine(first);
+Console.WriteLine(second);
+
+int i = 200;
+while (i > 0)
+{
+    double next = first + second;
+    Console.WriteLine(next);
+    first = second;
+    second = next;
+    i--;
+}
