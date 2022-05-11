48171002ceed254d30e3af7621abad0aff78744f
Artur-by <artur.by@mail.ru>
2022-05-11 12:59:31 +0300

    Task7 For Lead solved

---
+++ For Lead/Task7/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу копирования массива
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// создаем массив из рандомных n чисел
+int[] arr = new int[n];
+for (int i = 0; i < n-1; i++)
+{
+    arr[i] = new Random().Next(-10,10);
+}
+
+// создаем новый массив и копируем его элементы
+int[] newarr = new int[arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+    newarr[i] = arr[i];
+}
+
+// метод вывода элементов на печать
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} |");
+    }
+}
+
+Console.WriteLine("Созданный массив: ");
+PrintArr(arr);
+Console.WriteLine();
+Console.WriteLine("Скопированный массив: ");
+PrintArr(newarr);
